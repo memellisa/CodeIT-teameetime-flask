d6a4104bf1f0f72a52043903e6238b31c98c4632
vieriharney <vieri.harney@xcalibyte.com>
2021-09-25 16:48:27 +0800

    ads

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -12,7 +12,9 @@ logger = logging.getLogger(__name__)
 def evaluateTicTacToe():
     data = request.get_json()
 
-    event = requests.get("https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + data["battleId"])
+    headers = {'Accept': 'text/event-stream'}
+
+    event = requests.get("https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + data["battleId"], stream=True, headers=headers)
 
     print(data)
     print(event)
