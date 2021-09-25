3d95e2d9d4ffeb65711df3da0ea6162161e1903d
vieriharney <vieri.harney@xcalibyte.com>
2021-09-26 03:36:07 +0800

    update stock hunt

--- codeitsuisse/routes/stockhunter.py
+++ codeitsuisse/routes/stockhunter.py
@@ -47,24 +47,13 @@ def evaluateStockHunter():
         ### Finding minimum distance 
         gridMap = [grid[key] for key in grid]
 
-        def findPath(dict, entry, target):
-            from heapq import heappush, heappop
-            frontier = []
-            heappush(frontier, (0,[(),entry]))
-            explored = set()
-            while frontier:
-                node = heappop(frontier)
-                if node[1][-1] == target:
-                    return node[0]
-                if node[1][-1] not in explored:
-                    print('Exploring:',node[1][-1],'at cost',node[0])
-                    explored.add(node[1][-1])
-                    for child in neighbors[node[1][-1]]:
-                        newNode = node[1].copy()
-                        newNode.append(child[1])
-                        heappush(frontier, (node[0]+child[0],newNode))
+        newGrid= getNewGrid(gridMap)
+        neighbors = getNeighbors(newGrid)
+        print(neighbors)
+        minCost = findPath(neighbors,(0,0),(2,2))
+        print("Min cost ",minCost)
 
-        temp_dict["minimumCost"] = 9
+        temp_dict["minimumCost"] = minCost
         result.append(temp_dict)
 
     return jsonify(result)
@@ -93,3 +82,62 @@ def find_ri(tup, entryPoint, targetPoint, horizontalStepper, verticalStepper, rl
     return tup[1] * verticalStepper
   else:
     return get_rl((tup[0] - 1, tup[1]), rl) * get_rl((tup[0], tup[1] - 1), rl)
+
+def findPath(neighbors, entry, target):
+    from heapq import heappush, heappop
+    frontier = []
+    heappush(frontier, (0,[(),entry]))
+    explored = set()
+    while frontier:
+        node = heappop(frontier)
+        if node[1][-1] == target:
+            return node[0]
+        if node[1][-1] not in explored:
+            print('Exploring:',node[1][-1],'at cost',node[0])
+            explored.add(node[1][-1])
+            for child in neighbors[node[1][-1]]:
+                newNode = node[1].copy()
+                newNode.append(child[1])
+                heappush(frontier, (node[0]+child[0],newNode))
+
+def convertToCost(letter):
+    if letter == "L":
+        return 3
+    elif letter == "M":
+        return 2
+    elif letter == "S":
+        return 1
+
+def getNewGrid(grid):
+    newColLength = len(grid[0]) + 2
+    newGrid = []
+    newGrid.append([0] * newColLength)
+    for i in range(len(grid)):
+        newCol = [0]
+        for j in range(len(grid[0])):
+            newCol.append(grid[i][j])
+        newCol.append(0)
+        newGrid.append(newCol)
+    newGrid.append([0] * newColLength)
+    return newGrid
+
+def getNeighbors(grid):
+    neighbors={}
+    for i in range(1,len(grid)-1):
+        for j in range(1,len(grid[0])-1):
+            directions = {
+                (i-2,j-1):grid[i-1][j],
+                (i,j-1):grid[i+1][j],
+                (i-1,j):grid[i][j+1],
+                (i-1,j-2):grid[i][j-1]
+                }
+            list = []
+            for key,value in directions.items():
+                cost = convertToCost(value)
+                if value != 0:
+                    list.append((cost,key))
+            real_x = i-1
+            real_y = j-1
+            neighbors[(real_x,real_y)] = list
+
+    return neighbors
