8fc51d25adda8b5734deabc353a033d5148db364
vieriharney <vieri.harney@xcalibyte.com>
2021-09-25 16:23:13 +0800

    init tictactoe

---
+++ codeitsuisse/routes/tictactoe.py
@@ -0,0 +1,16 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/tic-tac-toe', methods=['POST'])
+def evaluateSquare():
+    data = request.get_json()
+    print(data)
+
+
+
