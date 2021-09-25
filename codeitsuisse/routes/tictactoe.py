3b9b727cb9e6b34463ff55c81695c3e2078250ac
vieriharney <vieri.harney@xcalibyte.com>
2021-09-25 16:37:06 +0800

    ad

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import requests
 
 from flask import request, jsonify
 
@@ -10,7 +11,10 @@ logger = logging.getLogger(__name__)
 @app.route('/tic-tac-toe', methods=['POST'])
 def evaluateTicTacToe():
     data = request.get_json()
-    print(data)
 
+    event = requests.get("https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + data["battleId"])
+
+    print(data)
+    print(event)
 
 
